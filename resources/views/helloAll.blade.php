6bf276b933e42f3d674068c9b985c5b3e4be0bbf
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2021-01-22 16:26:15 +0100

    helloAll

---
+++ resources/views/helloAll.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>helloAll</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{ asset('js/app.js') }}"></script>
+
+  </head>
+  <body>
+
+    <div class="container">
+      <h1>saluta tutti gli utenti in inglese (statico, no variabili) -   hello all users </h1>
+
+    </div>
+
+    {{-- <div class="pluto">
+      <h1>ciao mondo</h1>
+    </div> --}}
+
+  </body>
+</html>
